f2001c7c23c638ad2d8c01bec449a6c5d3dc1147
Mustafa Prodhan <mustafa.prodhan.317@gmail.com>
2021-07-06 23:42:50 -0400

    tested the email with straight code vs funtional coding. The straight code works.

---
+++ pymail.py
@@ -0,0 +1,24 @@
+import smtplib
+from email.message import EmailMessage
+from string import Template
+from pathlib import Path
+
+
+html = Template(Path('index.html').read_text())
+email = EmailMessage()
+email['from'] = 'mufassaproductions@gmail.com'
+# email['to'] = 'mustafizur.prodhan2@gmail.com'
+email['bcc'] = 'mustafizur.prodhan@gmail.com', \
+                    'mustafizur.prodhan2@gmail.com'
+email['subject'] = 'Test'
+
+email.set_content('This is a test of your TV! We control the vertical, the horizontal.')
+email.set_content(html.substitute({'name': 'Chobi'}), 'html')
+
+
+with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.login('mufassaproductions@gmail.com', '41810568a!')
+    smtp.send_message(email)
+    print('sent')
